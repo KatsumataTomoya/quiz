d96b15c3131a59bc1ad35bfd3a0272d7db33d4fe
Tomoya Katsumata <s1F102000905@iniad.org>
2023-09-21 14:20:49 +0900

    Create q.js

---
+++ q.js
@@ -0,0 +1,29 @@
+// XMLHttpRequestを使ってjsonデータを読み込む
+let requestURL = '50.json';//jsonへのパス
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+ 
+// JSONデータをJavaScriptオブジェクトに変換
+request.onload = function () {
+  let data = request.response;
+  data = JSON.parse(JSON.stringify(data));
+  dataArray(data);
+}
+ 
+// foreachでJSONデータをHTMLに出力
+function dataArray(els) {
+  // JSONデータを出力したいHTML要素を指定
+  let array = document.querySelector('.array');
+  els.forEach(el => {
+    let id = el.id;
+    let original_text = el.original_text;
+
+    let code = '<ul>'+
+    '<li>名前：' + id + '</li>'+
+    '<li>学年：' + original_text + '</li>'+
+    '</ul>';
+    array.insertAdjacentHTML('beforeend',code);
+  });
+}
