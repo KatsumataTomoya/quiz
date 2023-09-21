2ad28cd817dff639f8e4488d5a51e2a175935001
Tomoya Katsumata <s1F102000905@iniad.org>
2023-09-21 16:22:17 +0900

    Update q.js

--- q.js
+++ q.js
@@ -1,29 +1,19 @@
-// XMLHttpRequestを使ってjsonデータを読み込む
-let requestURL = '50.json';//jsonへのパス
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
- 
-// JSONデータをJavaScriptオブジェクトに変換
-request.onload = function () {
-  let data = request.response;
-  data = JSON.parse(JSON.stringify(data));
-  dataArray(data);
-}
- 
-// foreachでJSONデータをHTMLに出力
-function dataArray(els) {
-  // JSONデータを出力したいHTML要素を指定
-  let array = document.querySelector('.array');
-  els.forEach(el => {
-    let id = el.id;
-    let original_text = el.original_text;
+let jsonURL = '50.json'; // JSONファイルのパスを指定
 
-    let code = '<ul>'+
-    '<li>名前：' + id + '</li>'+
-    '<li>学年：' + original_text + '</li>'+
-    '</ul>';
-    array.insertAdjacentHTML('quiz',code);
-  });
+// JSONデータを取得し、表示する関数
+function displayJSONData(data) {
+    let jsonContent = document.getElementById('quiz');
+    
+    // JSONデータを文字列に変換して表示
+    jsonContent.textContent = JSON.stringify(data, null, 2);
 }
+
+// JSONデータを非同期で読み込む
+fetch(jsonURL)
+    .then(response => response.json())
+    .then(data => {
+        displayJSONData(data);
+    })
+    .catch(error => {
+        console.error('JSONデータの読み込みエラー:', error);
+    });
